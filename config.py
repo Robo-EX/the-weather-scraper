ead320a3913a51d1cf1c9d6b556c0cc1bd48c607
Bhavik Mukesh Panchal <bmp7963@nyu.edu>
2022-05-24 10:19:18 -0400

    added python book

--- config.py
+++ config.py
@@ -1,8 +1,25 @@
-from datetime import date
+#!/usr/bin/env python3
+from datetime import datetime, timedelta, date
+import re
+
 
 # Set Date format like: YYYY, MM, DD
 # Note that FIND_FIRST_DATE uses START_DATE as default start date
-START_DATE = date(2020, 10, 1)
+avg_over_min = 1
+days_look_back = 40
+en_date = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+st_date = (datetime.now() - timedelta(days=days_look_back)
+           ).strftime('%Y-%m-%dT%H:%M:%S')
+
+# searching string
+match_str = re.search(r'\d{4}-\d{2}-\d{2}', st_date)
+
+# computed date
+# feeding format
+res = datetime.strptime(match_str.group(), '%Y-%m-%d').date()
+
+
+START_DATE = res
 END_DATE = date.today()
 
 # set to "metric" or "imperial"
